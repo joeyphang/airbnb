2fbf64a916f33e6ea6906e28562374a2e7a8c80f
joeyphang <joeyphangx@gmail.com>
2018-07-09 15:07:52 +0800

    added edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
 
-  
-resources :listings #all crud restful routes
+
+resources :listings # creates all crud restful routes
 end
